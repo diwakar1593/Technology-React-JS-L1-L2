b9a12ab3dd26e8d96a37fa74efae85a1fb08253c
diwakar1593 <diwakar200kumar@gmail.com>
2025-09-05 23:47:39 +0530

    react router

--- src/16_reactrouter/AddCommentPage.jsx
+++ src/16_reactrouter/AddCommentPage.jsx
@@ -1,14 +1,14 @@
 // Implementation using functional component / React Hooks is much simpler
 
 import React from "react";
-import { useNavigate } from 'react-router-dom';     // useNavigate is a React Hook provided by react-router-dom
+import { data, useLocation, useNavigate } from 'react-router-dom';     // useNavigate is a React Hook provided by react-router-dom
 import CommentForm from './CommentForm';
 import CommentApi from './data/CommentApi';
 
 
 // Container Component is one which interacts with model
 // easier one - follow this.
-
+/*
 export default () => {
     const navigate = useNavigate();
 
@@ -20,3 +20,30 @@ export default () => {
         <CommentForm onSave={(data) => saveComment(data)} />
     )
 }
+*/
+
+// In case you want to use class component with router, then below
+function withRouter(Child) {
+    return (props) => {
+        const location = useLocation();
+        const navigate = useNavigate();
+        return <Child {...props} navigate={navigate} location={location} />;
+    }
+}
+
+// Container Component
+class AddCommentPage extends React.Component {
+    saveComment(comment) {
+        CommentApi.saveComment(comment);    // saving data into model
+        this.props.navigate('/');
+    }
+
+    render() {
+        return (
+            <CommentForm onSave={(data) => this.saveComment(data)} />
+        );
+    }
+}
+
+// HOC - Higher Order Component 
+export default withRouter(AddCommentPage);
